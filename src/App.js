09803545fa7b4ddea89ea3fe56b8c618c986ce7a
Andy Strube <andystrube64@gmail.com>
2021-07-06 16:25:16 -0700

    Moved input form to its own component

--- src/App.js
+++ src/App.js
@@ -1,51 +1,12 @@
-import './App.css';
-import axios from "axios"
-import React, { useState } from "react";
+import InputForm from './InputForm';
 
 function App() {
-  const [input, setInput] = useState('');
+  
+  const inputForm = <InputForm/>
 
-  const submitComment = (formData) => {
-    //axios.post(`http://localhost:3000/tasks`, formData)
-    axios.post(`https://andy-todo-list-back-end.herokuapp.com/tasks`, formData)
-    .catch((err) => console.log(err.response.data));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if(input.length > 0) {
-      submitComment({ content: input });
-      setInput('');
-    } else {
-      alert("Comments can't be blank");
-    }
-  };
-
-  const inputButton = () => {
-    return <input
-      type="submit"
-      value="Submit"
-      className=""
-    />
-  };
-
-  const inputElement = () => {
-    return <input
-      type="text"
-      placeholder="Add a comment"
-      id="input-element"
-      size="30"
-      value={input}
-      onChange={e => setInput(e.target.value)}
-      className="field"
-    />
-  };
-
-  return <form onSubmit={handleSubmit}>
-    {inputElement()}
-    <br/><br/>
-    {inputButton()}
-  </form>
+  return <div>
+    {inputForm}
+  </div>
 }
 
 export default App;
---
+++ src/InputForm.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+function InputForm() {
+  const [input, setInput] = useState('');
+
+  const submitComment = (formData) => {
+    //axios.post(`http://localhost:8000/tasks`, formData)
+    axios.post(`https://andy-todo-list-back-end.herokuapp.com/tasks`, formData)
+    .catch((err) => console.log(err.response.data));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(input.length > 0) {
+      submitComment({ content: input });
+      setInput('');
+    } else {
+      alert("Comments can't be blank");
+    }
+  };
+
+  const inputButton = () => {
+    return <input
+      type="submit"
+      value="Submit"
+      className=""
+    />
+  };
+
+  const inputElement = () => {
+    return <input
+      type="text"
+      placeholder="Add a comment"
+      id="input-element"
+      size="30"
+      value={input}
+      onChange={e => setInput(e.target.value)}
+      className="field"
+    />
+  };
+
+  return <form onSubmit={handleSubmit}>
+    {inputElement()}
+    <br/><br/>
+    {inputButton()}
+  </form>
+}
+
+export default InputForm;
